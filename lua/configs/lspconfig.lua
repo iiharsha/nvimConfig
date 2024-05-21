3a56abed98b61193503a417d39c9105c31213178
iiharsha <iiharsha10@gmail.com>
2024-05-21 21:58:18 +0530

    idk maybe it worked

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "clangd", "gopls", "jsonnet_ls", "lua_ls", "pyright", "rust_analyzer",  "tailwindcss", "tsserver","jdtls"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
